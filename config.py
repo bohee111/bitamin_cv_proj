44f6f7604aba3d4ade5079ce6b8706cb77235361
bohee111 <bboohhee06@gmail.com>
2026-01-20 13:11:57 +0900

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ import os
 '''
     define your root directory
 '''
-ROOT = './dataset'
-
- 
+# config.py
+ROOT = "/kaggle/input/parkbohee/AnimalCLEF2025-main/dataset"  # 실제 dataset 폴더 위치에 맞춰 수정
+OUT_DIR = "/kaggle/working/out"
+DEVICE = "cuda"
 
 # Model settings
 MEGAD_NAME = 'hf-hub:BVRA/MegaDescriptor-L-384'
